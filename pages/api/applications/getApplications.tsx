cd4de12846c04e6d1d8f61d19fc46333419517a2
Jennifer <thejenniferhaggerty@gmail.com>
2025-03-20 13:31:54 -0400

    Fix collections

--- pages/api/applications/getApplications.tsx
+++ pages/api/applications/getApplications.tsx
@@ -1,11 +1,10 @@
-import { ObjectId } from 'bson';
 import clientPromise from '../../../lib/mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
 import JobInterface from '@/interfaces/JobInterface';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const client = await clientPromise;
   const db = await client.db(process.env.MONGODB);
-  const collection = process.env.MONGODB_COLLECTION_USERS;
+  const collection = process.env.MONGODB_COLLECTION_APPLICATIONS;
   if (!db || !collection) {
     return res.status(500).json({
       message: `The database or applications collection is missing. Check your setup.`,
