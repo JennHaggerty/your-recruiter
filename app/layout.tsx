9490a76bde37a7dee89ef32450918af92725625e
Jennifer <thejenniferhaggerty@gmail.com>
2025-03-27 16:09:32 -0400

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/styles/globals.css';
 import strings from '@/common/strings';
+import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: strings.siteTitle,
@@ -14,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
